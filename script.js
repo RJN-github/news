2137fb96edebf9807a75389799e8d7bc5820c23d
Rishikesh <159117353+RJN-github@users.noreply.github.com>
2024-09-03 23:04:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const apikey = "f2241965ed514ba09b12f8157d4df511"
-const apikey1 = "2ad8760105dd4b34bdc697a76d847767"
+const apikey = "a222e3a85c33226599e75662828964cd"
+const apikey1 = "98d3c32d76736fa6d44a9628e41c72b3"
 const api = "&sortBy=publishedAt"
-const apiurl = "https://newsapi.org/v2/everything?q="
+const apiurl = "https://gnews.io/api/v4/search?q="
 const searchtext = document.querySelector(".search-text")
 const searchbtn = document.querySelector(".search-btn")
 const container = document.querySelector(".container")
 
+
+
 window.addEventListener('load', () => {
     loadnews("India")
+    console.log("page loaded with india as query");
+    
+})
+
+document.querySelector(".logo").addEventListener('click',()=>{
+    window.location.reload()
 })
 
 document.querySelector(".Global").addEventListener('click', () => {
@@ -19,19 +27,19 @@ document.querySelector(".Global").addEventListener('click', () => {
 document.querySelector(".Sports").addEventListener('click', () => {
     console.log("Sports clicked");
     container.innerHTML = ""
-    loadnews("sports +football +cricket +olympic")
+    loadnews("sports football cricket olympic")
 })
 
 document.querySelector(".Finance").addEventListener('click', () => {
     console.log("finance clicked");
     container.innerHTML = ""
-    loadnews("Finance +india")
+    loadnews("Finance india")
 })
 
 document.querySelector(".Politics").addEventListener('click', () => {
     console.log("Politics clicked");
     container.innerHTML = ""
-    loadnews("Politics +india +war")
+    loadnews("Politics india war")
 })
 
 searchbtn.addEventListener('click', () => {
@@ -40,7 +48,7 @@ searchbtn.addEventListener('click', () => {
 })
 
 async function loadnews(query) {
-    let response = await fetch(`${apiurl}${query}${api}&apikey=${apikey}`)
+    let response = await fetch(`${apiurl}${query}&lang=en${api}&apikey=${apikey}`)
 
     if (!response.ok) {
         console.log("1st api key failed");
@@ -49,7 +57,8 @@ async function loadnews(query) {
     let container = document.querySelector(".container")
     let data = await response.json()
     console.log(data.articles);
-    if(data.articles.length===0){
+    if(data.articles.length==="" || data.articles===undefined){
+        alert('no news on your search')
         container.innerHTML = 'no any recent news on your search'
     }
     
@@ -57,7 +66,7 @@ async function loadnews(query) {
 
         container.innerHTML = container.innerHTML + `<a href="${data.articles[index].url}" class = "card-cont">
         <div class="card-img">
-        <img class ="image" src="${data.articles[index].urlToImage}" alt="Photo Unavailable">
+        <img class ="image" src="${data.articles[index].image}" alt="Photo Unavailable">
         </div>
         <div class="card-text">
         <div class="title">${data.articles[index].title}</div>
